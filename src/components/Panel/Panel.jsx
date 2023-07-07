b1e603516ddd3771d20c68b3697dd4d2adb1e880
Ajarek <ajarek@poczta.onet.pl>
2023-07-07 21:20:18 +0200

    added component Panel

--- src/components/Panel/Panel.jsx
+++ src/components/Panel/Panel.jsx
@@ -1,26 +1,39 @@
+import { useState } from 'react'
 import './Panel.css'
 
 const Panel = () => {
+  const [selectedBackground, setSelectedBackground] = useState('yellow')
+  
   return (
     <div className='panel'>
-      <div  className='item item-panel'>
-          <div className='circle' style={{background:'red'}}></div>
-        </div>
-      <div  className='item item-panel'>
-          <div className='circle' style={{background:'blue'}}  ></div>
-        </div>
-      <div  className='item item-panel'>
-          <div className='circle' style={{background:'green'}} ></div>
-        </div>
-      <div  className='item item-panel'>
-          <div className='circle' style={{background:'yellow'}}  ></div>
-        </div>
-      <div  className='item item-panel'>
-          <div className='circle' style={{background:'black'}}  ></div>
-        </div>
-      <div  className='item item-panel'>
-          <div className='circle' style={{background:'white'}}  ></div>
-        </div>
+      <div className='item item-panel'>
+        <div
+          className='circle'
+          style={{ background: 'red' }}
+          onClick={(e) => setSelectedBackground(e.target.style.background)}
+        ></div>
+      </div>
+      <div className='item item-panel'>
+        <div
+          className='circle'
+          style={{ background: 'blue' }}
+          onClick={(e)=>setSelectedBackground(e.target.style.background)}
+        ></div>
+      </div>
+      <div className='item item-panel'>
+        <div
+          className='circle'
+          style={{ background: 'green' }}
+          onClick={(e)=>setSelectedBackground(e.target.style.background)}
+        ></div>
+      </div>
+      <div className='item item-panel'>
+        <div
+          className='circle'
+          style={{ background: 'yellow' }}
+          onClick={(e)=>setSelectedBackground(e.target.style.background)}
+        ></div>
+      </div>
     </div>
   )
 }
