563ab5cae111ffef607e3764befd58de578e94bf
Ajarek <ajarek@poczta.onet.pl>
2023-07-08 00:58:00 +0200

    added helper CompareArray

---
+++ src/helper/CompareArray.jsx
@@ -0,0 +1,18 @@
+export const compareArray=(arrayRandom,arrayUser)=>{
+    let newArray=[]
+   
+      for(let i=0; i<arrayRandom.length; i++)
+     { 
+      if(arrayUser[i]===arrayRandom[i]){
+       newArray.push('black')
+      }
+      else if(arrayRandom.includes(arrayUser[i])){
+        newArray.push('white')
+      }
+      else{
+        newArray.push('')
+    }
+    }
+    
+    return newArray
+  }
