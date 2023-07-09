d690285862337f57af21dcbd8c5b039841a2eab4
Ajarek <ajarek@poczta.onet.pl>
2023-07-09 21:43:37 +0200

    added logic in PanelInfo

--- src/components/PanelInfo/PanelInfo.jsx
+++ src/components/PanelInfo/PanelInfo.jsx
@@ -3,20 +3,102 @@ import { AppContext } from '../../App'
 import './PanelInfo.css'
 
 const PanelInfo = () => {
-  const { selectedBackground, setSelectedBackground, arrayAllInfo, setArrayAllInfo } = useContext(AppContext)
+  const {
+    selectedBackground,
+    setSelectedBackground,
+    arrayAllInfo,
+    setArrayAllInfo,
+  } = useContext(AppContext)
   return (
     <div className='panel-info'>
-     {arrayAllInfo?.map((info, index) =>{
-      return(
-        <div className="item">
-          {arrayAllInfo[0]?.map((item,ind) =>{
-            return(
-              <div key={ind} className="circle" style={{background:item}}></div>
-            )
-          })}
-        </div>
-      )
-     })}
+      <div className='item'>
+        {arrayAllInfo[0]?.map((item, ind) => {
+          return (
+            <div
+              key={ind}
+              className='circle'
+              style={{ background: item }}
+            ></div>
+          )
+        })}
+      </div>
+      <div className='item'>
+        {arrayAllInfo[1]?.map((item, ind) => {
+          return (
+            <div
+              key={ind}
+              className='circle'
+              style={{ background: item }}
+            ></div>
+          )
+        })}
+      </div>
+      <div className='item'>
+        {arrayAllInfo[2]?.map((item, ind) => {
+          return (
+            <div
+              key={ind}
+              className='circle'
+              style={{ background: item }}
+            ></div>
+          )
+        })}
+      </div>
+      <div className='item'>
+        {arrayAllInfo[3]?.map((item, ind) => {
+          return (
+            <div
+              key={ind}
+              className='circle'
+              style={{ background: item }}
+            ></div>
+          )
+        })}
+      </div>
+      <div className='item'>
+        {arrayAllInfo[4]?.map((item, ind) => {
+          return (
+            <div
+              key={ind}
+              className='circle'
+              style={{ background: item }}
+            ></div>
+          )
+        })}
+      </div>
+      <div className='item'>
+        {arrayAllInfo[5]?.map((item, ind) => {
+          return (
+            <div
+              key={ind}
+              className='circle'
+              style={{ background: item }}
+            ></div>
+          )
+        })}
+      </div>
+      <div className='item'>
+        {arrayAllInfo[6]?.map((item, ind) => {
+          return (
+            <div
+              key={ind}
+              className='circle'
+              style={{ background: item }}
+            ></div>
+          )
+        })}
+      </div>
+      <div className='item'>
+        {arrayAllInfo[7]?.map((item, ind) => {
+          return (
+            <div
+              key={ind}
+              className='circle'
+              style={{ background: item }}
+            ></div>
+          )
+        })}
+      </div>
     </div>
   )
 }
